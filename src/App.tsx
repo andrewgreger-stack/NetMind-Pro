1467ac2e36f856d79147bfafc193738929468963
andrewgreger-stack <andrew.greger@gmail.com>
2026-07-02 11:19:55 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ import { getAuth, signInWithCustomToken, signInAnonymously, onAuthStateChanged }
 import { getFirestore, collection, doc, setDoc, updateDoc, onSnapshot, deleteDoc } from 'firebase/firestore';
 
 // --- FIREBASE SETUP ---
-/*const firebaseConfig = JSON.parse(typeof __firebase_config !== 'undefined' ? __firebase_config : '{}');
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const appId = typeof __app_id !== 'undefined' ? __app_id : 'default-app-id';*/
+//const firebaseConfig = JSON.parse(typeof __firebase_config !== 'undefined' ? __firebase_config : '{}');
+//const app = initializeApp(firebaseConfig);
+//const auth = getAuth(app);
+//const db = getFirestore(app);
+//const appId = typeof __app_id !== 'undefined' ? __app_id : 'default-app-id';
 
 // --- MOCK DATA GENERATOR ---
 const generateMockData = () => {
